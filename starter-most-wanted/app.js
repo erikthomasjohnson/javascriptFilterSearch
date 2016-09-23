cc3ec6bf7deebd4a83fbd569e0cd07b377c077bd
erikthomasjohnson <erikjohnsoncomposer@gmail.com>
2016-09-23 08:48:19 -0500

    Filter and Next-of-Kin Working

---
+++ starter-most-wanted/app.js
@@ -0,0 +1,477 @@
+var dataObject = [{ 
+		"id":	"272822514",
+	  "firstName": "Billy",
+	  "lastName": "Bob",
+	  "gender": "male",
+	  "dob": "1/18/1949",
+	  "height": 71,
+	  "weight": 175,
+	  "eyeColor": "brown",
+	  "occupation": "programmer",
+	  "parents": [],
+	  "currentSpouse": 401222887 
+	},{
+		"id":	"401222887",
+	  "firstName": "Uma",
+	  "lastName": "Bob",
+	  "gender": "female",
+	  "dob": "4/1/1947",
+	  "height": 65,
+	  "weight": 162,
+	  "eyeColor": "brown",
+	  "occupation": "assistant",
+	  "parents": [],
+	  "currentSpouse": 272822514 
+	},{
+		"id":	"409574486",
+	  "firstName": "Michael",
+	  "lastName": "Walkens",
+	  "gender": "male",
+	  "dob": "5/9/1951",
+	  "height": 76,
+	  "weight": 250,
+	  "eyeColor": "brown",
+	  "occupation": "landscaper",
+	  "parents": [],
+	  "currentSpouse": 260451248 
+	},{
+		"id":	"260451248",
+	  "firstName": "Jon",
+	  "lastName": "Walkens",
+	  "gender": "male",
+	  "dob": "9/6/1945",
+	  "height": 62,
+	  "weight": 115,
+	  "eyeColor": "brown",
+	  "occupation": "assistant",
+	  "parents": [],
+	  "currentSpouse": 409574486 
+	},{
+		"id":	"629807187",
+	  "firstName": "Jack",
+	  "lastName": "Pafoy",
+	  "gender": "male",
+	  "dob": "3/16/1938",
+	  "height": 70,
+	  "weight": 207,
+	  "eyeColor": "black",
+	  "occupation": "nurse",
+	  "parents": [],
+	  "currentSpouse": 464142841 
+	},{
+		"id":	"464142841",
+	  "firstName": "Jen",
+	  "lastName": "Pafoy",
+	  "gender": "female",
+	  "dob": "4/10/1940",
+	  "height": 72,
+	  "weight": 256,
+	  "eyeColor": "black",
+	  "occupation": "student",
+	  "parents": [],
+	  "currentSpouse": 629807187 
+	},{
+		"id":	"982411429",
+	  "firstName": "Mister",
+	  "lastName": "Potatoo",
+	  "gender": "male",
+	  "dob": "12/18/1952",
+	  "height": 66,
+	  "weight": 170,
+	  "eyeColor": "hazel",
+	  "occupation": "architect",
+	  "parents": [],
+	  "currentSpouse": 595767575 
+	},{
+		"id":	"595767575",
+	  "firstName": "Missuz",
+	  "lastName": "Potatoo",
+	  "gender": "female",
+	  "dob": "10/28/1948",
+	  "height": 59,
+	  "weight": 137,
+	  "eyeColor": "blue",
+	  "occupation": "architect",
+	  "parents": [],
+	  "currentSpouse": 982411429 
+	},{
+		"id":	"693243224",
+	  "firstName": "Joy",
+	  "lastName": "Madden",
+	  "gender": "female",
+	  "dob": "4/20/1939",
+	  "height": 69,
+	  "weight": 199,
+	  "eyeColor": "hazel",
+	  "occupation": "doctor",
+	  "parents": [],
+	  "currentSpouse": 888201200
+	},{
+		"id":	"888201200",
+	  "firstName": "Mader",
+	  "lastName": "Madden",
+	  "gender": "male",
+	  "dob": "5/6/1937",
+	  "height": 76,
+	  "weight": 205,
+	  "eyeColor": "black",
+	  "occupation": "landscaper",
+	  "parents": [],
+	  "currentSpouse": 693243224
+	},{
+		"id":	"878013758",
+	  "firstName": "Jill",
+	  "lastName": "Pafoy",
+	  "gender": "Bob",
+	  "dob": "2/8/1972",
+	  "height": 74,
+	  "weight": 118,
+	  "eyeColor": "brown",
+	  "occupation": "programmer",
+	  "parents": [401222887,401222887],
+	  "currentSpouse": 294874671 
+	},{
+		"id":	"951747547",
+	  "firstName": "Ralph",
+	  "lastName": "Bob",
+	  "gender": "male",
+	  "dob": "12/23/1969",
+	  "height": 66,
+	  "weight": 179,
+	  "eyeColor": "blue",
+	  "occupation": "nurse",
+	  "parents": [401222887,401222887],
+	  "currentSpouse": 159819275 
+	},{
+		"id":	"159819275",
+	  "firstName": "Jasmine",
+	  "lastName": "Bob",
+	  "gender": "female",
+	  "dob": "12/18/1969",
+	  "height": 58,
+	  "weight": 156,
+	  "eyeColor": "blue",
+	  "occupation": "assistant",
+	  "parents": [409574486,260451248],
+	  "currentSpouse": 951747547 
+	},{
+		"id":	"348457184",
+	  "firstName": "Annie",
+	  "lastName": "Pafoy",
+	  "gender": "female",
+	  "dob": "11/4/1970",
+	  "height": 62,
+	  "weight": 235,
+	  "eyeColor": "hazel",
+	  "occupation": "landscaper",
+	  "parents": [629807187,464142841],
+	  "currentSpouse": null 
+	},{
+		"id":	"294874671",
+	  "firstName": "Dave",
+	  "lastName": "Pafoy",
+	  "gender": "male",
+	  "dob": "8/5/1967",
+	  "height": 61,
+	  "weight": 112,
+	  "eyeColor": "green",
+	  "occupation": "doctor",
+	  "parents": [629807187,464142841],
+	  "currentSpouse": 878013758 
+	},{
+		"id":	"931247228",
+	  "firstName": "Amii",
+	  "lastName": "Pafoy",
+	  "gender": "female",
+	  "dob": "3/13/1963",
+	  "height": 74,
+	  "weight": 184,
+	  "eyeColor": "brown",
+	  "occupation": "landscaper",
+	  "parents": [629807187,464142841],
+	  "currentSpouse": null 
+	},{
+		"id":	"822843554",
+	  "firstName": "Regina",
+	  "lastName": "Madden",
+	  "gender": "female",
+	  "dob": "7/26/1959",
+	  "height": 71,
+	  "weight": 249,
+	  "eyeColor": "brown",
+	  "occupation": "nurse",
+	  "parents": [693243224,888201200],
+	  "currentSpouse": null 
+	},{
+		"id":	"819168108",
+	  "firstName": "Hana",
+	  "lastName": "Madden",
+	  "gender": "female",
+	  "dob": "10/7/1953",
+	  "height": 70,
+	  "weight": 187,
+	  "eyeColor": "brown",
+	  "occupation": "politician",
+	  "parents": [693243224,888201200],
+	  "currentSpouse": null 
+	},{
+		"id":	"969837479",
+	  "firstName": "Eloise",
+	  "lastName": "Madden",
+	  "gender": "female",
+	  "dob": "12/11/1961",
+	  "height": 63,
+	  "weight": 241,
+	  "eyeColor": "brown",
+	  "occupation": "assistant",
+	  "parents": [693243224,888201200],
+	  "currentSpouse": null 
+	},{
+		"id":	"313207561",
+	  "firstName": "Mattias",
+	  "lastName": "Madden",
+	  "gender": "male",
+	  "dob": "2/19/1966",
+	  "height": 70,
+	  "weight": 110,
+	  "eyeColor": "blue",
+	  "occupation": "assistant",
+	  "parents": [693243224,888201200],
+	  "currentSpouse": 313997561 
+	},{
+		"id":	"313997561",
+	  "firstName": "Ellen",
+	  "lastName": "Madden",
+	  "gender": "female",
+	  "dob": "2/19/1970",
+	  "height": 67,
+	  "weight": 100,
+	  "eyeColor": "blue",
+	  "occupation": "doctor",
+	  "parents": [],
+	  "currentSpouse": 313207561 
+	},{
+		"id":	"313998000",
+	  "firstName": "Joey",
+	  "lastName": "Madden",
+	  "gender": "female",
+	  "dob": "2/02/1987",
+	  "height": 67,
+	  "weight": 100,
+	  "eyeColor": "blue",
+	  "occupation": "doctor",
+	  "parents": [313207561,313997561],
+	  "currentSpouse": null 
+	}
+];
+
+/*  Remove this from your final submission
+function printAllToConsole(dataObj){
+	for (var key in dataObj) {
+		if (dataObj.hasOwnProperty(key)) {
+			console.log(key + " -> " + JSON.stringify(dataObj[key]));
+		}
+	}
+}
+printAllToConsole(dataObject);
+*/
+
+function initSearch(){
+	var fName = prompt("First Name, Gender, DOB, Height, Weight, Eye Color, or Occupation: ");
+	if (isName(fName)){
+		var lName = displayObject(fName)[0].lastName; // prompt("Last Name: ");
+	}
+	var searchObject = displayObject(fName);
+	var subject = getPersonInfo( fName, lName);
+	var spouse = getSpouse(subject);
+	var children = getChildren(subject, fName);
+	var parents = getParents(subject);
+	var siblings = getChildren(parents, fName);
+	var grandChildren = getChildren(children, fName)
+	var grandParents = getParents(parents);
+	var nieceNephew = getChildren(siblings, fName)
+	var auntUncle = getChildren(grandParents, fName)
+	var family = getFamily(fName, lName);
+	var familyList = nextOfKin(subject, spouse, children, parents, siblings, grandChildren, grandParents, nieceNephew, auntUncle, fName);
+	// responder(subject);
+	// responder(parents);
+	// responder(grandParents);
+	// responder(siblings);
+	// responder(children);
+	// responder(family);
+	// responder(grandChildren);
+	// responder(searchObject)
+	responder(familyList);
+}
+function responder(results){
+	alert(JSON.stringify(results));
+}
+function isName(name){
+	for (var i = 0; i < dataObject.length; i++){
+		if (dataObject[i].firstName == name){
+			return true;
+		}
+	}
+	return false;
+}
+function displayObject(name){
+var subject = addObject(name);
+var newObject = [{"{{'''':''''}:''*****No Information to Display*****" : {"":""}}];
+var index = 0;
+if (subject[0].id > 0){ newObject[index++] = "Search Parameter: " + name.toUpperCase() + " ";
+for (var i = 0; i < subject.length; i++){
+	newObject[index++] = subject[i].firstName + "_" + subject[i].lastName + " ";}}
+return newObject;
+}
+function nextOfKin(subject, spouse, children, parents, siblings, grandChildren, grandParents, nieceNephew, auntUncle, fName){
+var newObject = [{"{{'''':''''}:''*****No Information to Display*****" : {"":""}}];
+var index = 0;
+	if (isName(fName) == true){
+		if (subject[0].id > 0){ newObject[index++] = "NAME";
+		newObject[index++] = " " + subject[0].firstName + "_" + subject[0].lastName;}
+		if (spouse[0].id > 0){ newObject[index++] = "     SPOUSE";
+		newObject[index++] = " " + spouse[0].firstName + "_" + spouse[0].lastName;}
+		if (children[0].id > 0){ newObject[index++] = "     CHILDREN";
+		for (var i = 0; i < children.length; i++){
+			newObject[index++] = " " + children[i].firstName + "_" + children[i].lastName;}}
+		if (parents[0].id > 0){ newObject[index++] = "     PARENTS";
+		for (var i = 0; i < parents.length; i++){
+			newObject[index++] = " " + parents[i].firstName + "_" + parents[i].lastName;}}
+		if (siblings[0].id > 0){ newObject[index++] = "     SIBLINGS";
+		for (var i = 0; i < siblings.length; i++){
+			newObject[index++] = " " + siblings[i].firstName + "_" + siblings[i].lastName;}}
+		if (grandChildren[0].id > 0){ newObject[index++] = "     GRANDCHILDREN";
+		for (var i = 0; i < grandChildren.length; i++){
+			newObject[index++] = " " + grandChildren[i].firstName + "_" + grandChildren[i].lastName;}}
+		if (grandParents[0].id > 0){ newObject[index++] = "     GRANDPARENTS";
+		for (var i = 0; i < grandParents.length; i++){
+			newObject[index++] = " " + grandParents[i].firstName + "_" + grandParents[i].lastName;}}
+		if (nieceNephew[0].id > 0){ newObject[index++] = "     NIECE/NEPHEW";
+		for (var i = 0; i < nieceNephew.length; i++){
+			newObject[index++] = " " + nieceNephew[i].firstName + "_" + nieceNephew[i].lastName;}}
+		if (auntUncle[0].id > 0){ newObject[index++] = "     AUNT/UNCLE";
+		for (var i = 0; i < auntUncle.length; i++){
+			newObject[index++] = " " + auntUncle[i].firstName + "_" + auntUncle[i].lastName;}}
+		return newObject;
+	}
+	else{
+		return displayObject(fName)
+	}
+}
+function getPersonInfo(firstname, lastname){
+	var subject = addObject(firstname);
+	return subject;
+}
+function getSpouse(subject){
+var newObject = [{"{{'''':''''}:''*****No Information to Display*****" : {"":""}}];
+var spouseId = subject[0].currentSpouse;
+	if (spouseId != null){
+		var spouse = addObject(spouseId);
+		spouse = removeObject(spouse, subject[0].firstName);
+		return spouse;
+	}
+	return newObject;
+}
+function getFamily(firstname, lastname){
+try {
+	var tempFamily = addObject(lastname);
+	var family = removeObject(tempFamily, firstname);
+	return family;
+	}
+	catch (error) {
+		console.log(error.message);
+		return;
+	}
+}
+function getChildren(subject, name){
+	var index = 0;
+	var newObject = [{"{{'''':''''}:''*****No Information to Display*****" : {"":""}}];
+	var number = 0;
+	try {
+		if (subject == undefined){subject = newObject;}
+		if (subject.length > 0){
+			for (var i = 0; i < subject.length; i++){
+				number = subject[i].id;
+				for (var key in dataObject) {
+					if (dataObject.hasOwnProperty(key)) {
+						if ((number > 0) && ((dataObject[key].parents[0] == number) || (dataObject[key].parents[1] == number))){
+							newObject[index++] = dataObject[key];
+						}
+					}
+				}
+			}
+			newObject = removeDuplicate(newObject);
+			newObject = removeObject(newObject, name);
+			return newObject;
+		}
+	}
+	catch (error) {
+		console.log(error.message);
+		return newObject;
+	}
+}
+function getParents(subject){
+	var newObject = [{"{{'''':''''}:''*****No Information to Display*****" : {"":""}}];
+	var number = 0;
+	try {
+		if (subject == undefined){subject = newObject;}
+		if (subject.length > 0){
+			for (var ii = 0; ii < subject.length; ii++){
+				if (subject[ii].parents == undefined){subject[ii].parents = newObject;}
+				for (var i = 0; i < subject[ii].parents.length; i++){
+					number = subject[0].parents[i];
+						newObject[i] = addObject(number)[i];
+				}
+			}
+			return newObject;
+		}
+	}
+	catch (error) {
+		console.log(error.message);
+		return newObject;
+	}
+}
+function addObject(name){
+	var i = 0;
+	var newObject = [{"{{'''':''''}:''*****No Information to Display*****" : {"":""}}];
+	for (var key in dataObject) {
+		if (dataObject.hasOwnProperty(key)) {
+			for (var value in dataObject[key]){
+				if (dataObject[key].hasOwnProperty(value)){
+					if (dataObject[key][value] == name){
+						newObject[i++] = dataObject[key];
+					}
+				}
+			}
+		}
+	}
+	return newObject;
+}
+function removeObject(object, name){
+	var i = 0;
+	var newObject = [{"{{'''':''''}:''*****No Information to Display*****" : {"":""}}];
+		getkey: for (var key in object) {
+			if (object.hasOwnProperty(key)) {
+				for (var value in object[key]){
+					if (object[key].hasOwnProperty(value)){
+						if (object[key][value] == name){
+							continue getkey;
+						}
+					}
+				}
+			newObject[i++] = object[key];
+			}
+		}
+	return newObject;
+}
+function removeDuplicate(object){
+	var index = 0;
+	var newObject = [{"{{'''':''''}:''*****No Information to Display*****" : {"":""}}];
+	while (object[0].id > 0){
+		newObject[index++] = object[0]
+		object = removeObject(object, object[0].firstName);
+	}
+	return newObject;
+}
+initSearch();
+//window.close(); // exit window as the end of the session -- you may remove this
